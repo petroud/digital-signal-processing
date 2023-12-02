c08ac94497aee9cde983a67f16e93551dec0834a
Dimitris Petrou <petroudimitris@outlook.com>
2023-12-02 23:23:06 +0200

    Implemented subsampled filtering q3

--- lab4/src/lab4_ex2.m
+++ lab4/src/lab4_ex2.m
@@ -159,5 +159,72 @@ title('X(F) filtered using Hanning with N=41');
 
 %% Question (c)
 
-Fs_C = 100; %Sampling Frequency
-Ts_B = 1/Fs_B; 
+Fs_C = 50; %Sampling Frequency
+Ts_C = 1/Fs_C;
+
+%Set the sample space
+N  = 500;
+n = 0:N-1;
+
+%Set the signal
+x = sin(15*n*Ts_C) + 0.25*sin(200*n*Ts_C);
+
+
+%Set the new frequency axis needed after the Fourier Transform and the
+%applied sampling
+f_axis = -Fs_C/2:Fs_C/N:Fs_C/2-Fs_C/N;
+
+%Calculate the FT of x(t)
+X = fftshift(fft(x));
+
+%Filter the signal using the 4 filters
+x_hamm21 = filter(hamm21, 1, x);
+x_hamm41 = filter(hamm41, 1, x);
+x_hann21 = filter(hann21, 1, x);
+x_hann41 = filter(hann41, 1, x);
+
+%Calculate FT of each filtered result to get the spectrum
+X_mm21 = fftshift(fft(x_hamm21));
+X_mm41 = fftshift(fft(x_hamm41));
+X_nn21 = fftshift(fft(x_hann21));
+X_nn41 = fftshift(fft(x_hann41));
+
+figure()
+subplot(3,1,1);
+plot(f_axis, abs(X));
+xlabel('F(Hz)');
+ylabel('|X(F)|')
+title('Spectrum of x(t)(X(F)) at F_s=50Hz');
+
+subplot(3,1,2);
+plot(f_axis, abs(X_mm21));
+xlabel('F(Hz)');
+ylabel('|X(F)|');
+title('X(F) filtered using Hamming with N=21');
+
+subplot(3,1,3);
+plot(f_axis, abs(X_mm41));
+xlabel('F(Hz)');
+ylabel('|X(F)|');
+title('X(F) filtered using Hamming with N=41');
+
+
+figure()
+subplot(3,1,1);
+plot(f_axis, abs(X));
+xlabel('F(Hz)');
+ylabel('|X(F)|')
+title('Spectrum of x(t)(X(F)) at F_s=50Hz');
+
+subplot(3,1,2);
+plot(f_axis, abs(X_nn21));
+xlabel('F(Hz)');
+ylabel('|X(F)|');
+title('X(F) filtered using Hanning with N=21');
+
+subplot(3,1,3);
+plot(f_axis, abs(X_nn41));
+xlabel('F(Hz)');
+ylabel('|X(F)|');
+title('X(F) filtered using Hanning with N=41');
+
