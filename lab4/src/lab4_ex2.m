5c0d8b91777c09d3afe8e4f04d55dbe2179b161e
Dimitris Petrou <petroudimitris@outlook.com>
2023-12-02 22:42:04 +0200

    Init Exercise 2 source

---
+++ lab4/src/lab4_ex2.m
@@ -0,0 +1,30 @@
+clear all;
+close all;
+clc;
+
+% Exercise 2 
+
+
+%% Creation of common constants for all three questions
+omega_c = 0.5*pi;
+Fc = omega_c/(2*pi);
+Nsmall = 21; 
+Nbig = 41;
+
+
+%% Question (a)
+
+Fs_A = 100; %Sampling Frequency
+Ts_A = 1/Fs_A; 
+
+
+%% Question (b)
+
+Fs_B = 100; %Sampling Frequency
+Ts_B = 1/Fs_B;
+
+
+%% Question (c)
+
+Fs_C = 100; %Sampling Frequency
+Ts_B = 1/Fs_B; 
